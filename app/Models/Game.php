e6e906dbd495dbd0eda666668b0983fd3404815e
John Drexler <johnrudolphdrexler@gmail.com>
2024-12-30 09:52:21 -0500

    WIP

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -39,7 +39,9 @@ class Game extends Model
         
         $victory_shape = $is_bot_game
             ? 'square'
-            : collect(['square', 'line', 'el', 'zig'])->random();
+            : collect(['square', 'line', 'el', 'zig', 'pyramid'])->random();
+
+        $victory_shape = 'pyramid';
 
         PlayerCreated::fire(
             game_id: $game_id,
