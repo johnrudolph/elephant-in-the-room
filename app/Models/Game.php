f202c025627573b5eed644d0aedf4cd496e50128
John Drexler <johnrudolphdrexler@gmail.com>
2025-01-03 11:05:27 -0500

    WIP

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -37,7 +37,9 @@ class Game extends Model
             is_rematch_from_game_id: $is_rematch_from_game_id,
         )->game_id;
         
-        $victory_shape = collect(['square', 'line', 'el', 'zig', 'pyramid'])->random();
+        $victory_shape = $is_bot_game 
+            ? collect(['square', 'line', 'el'])->random()
+            : collect(['square', 'line', 'el', 'zig', 'pyramid'])->random();
 
         PlayerCreated::fire(
             game_id: $game_id,
